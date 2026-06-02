4b2b1b129f79d9c7b33f35f3b53641cf1d237d88
umandathathsarani <umathathsarani2003@gmail.com>
2026-06-02 13:40:44 +0530

    Complete database client implementation and verify connection to Atlas

--- integrations/db_client.py
+++ integrations/db_client.py
@@ -1,13 +1,19 @@
 import os
+import json
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 
-load_dotenv()
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+dotenv_path = os.path.join(base_dir, '.env')
+load_dotenv(dotenv_path)
 
 class DatabaseClient:
     def __init__(self):
         self.uri = os.getenv("MONGODB_URI")
+        if not self.uri:
+            raise ValueError("Critical Error: MONGODB_URI not found in environment variables!")
+        
         self.client = MongoClient(self.uri, server_api=ServerApi('1'))
         self.db = self.client["interactive_fiction"]
         self.collection = self.db["story_nodes"]
@@ -18,3 +24,21 @@ class DatabaseClient:
     def insert_nodes(self, nodes_data: list):
         if nodes_data:
             self.collection.insert_many(nodes_data)
+
+if __name__ == "__main__":
+    try:
+        client = DatabaseClient()
+        
+        json_path = os.path.join(base_dir, "data", "story_nodes.json")
+        
+        with open(json_path, 'r') as file:
+            raw_story = json.load(file)
+        
+        nodes_list = list(raw_story.values())
+        
+        client.collection.delete_many({})
+        client.insert_nodes(nodes_list)
+        print("Story nodes successfully uploaded to MongoDB Atlas!")
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
